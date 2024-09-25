07f58ef543442c04dd01e54fa040e8128ba8b5be
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2024-09-24 18:24:16 -0700

    Initial commit

---
+++ PdfPrintScaler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PdfPrintScaler
+//
+//  Created by Stephen Bensley on 9/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PdfPrintScaler/PdfPrintScalerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfPrintScalerApp.swift
+//  PdfPrintScaler
+//
+//  Created by Stephen Bensley on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PdfPrintScalerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
