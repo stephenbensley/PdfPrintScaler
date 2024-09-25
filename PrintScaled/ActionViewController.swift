a44357af7cf692ff4098f670164c18d95e20ddb2
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2024-09-24 18:55:23 -0700

    Action Extension boilerplate

--- PrintScaled/ActionViewController.swift
+++ PrintScaled/ActionViewController.swift
@@ -5,53 +5,72 @@
 // license at https://github.com/stephenbensley/PdfPrintScaler/blob/main/LICENSE.
 //
 
+import SwiftUI
 import UIKit
-import MobileCoreServices
 import UniformTypeIdentifiers
 
-class ActionViewController: UIViewController {
+struct ContentView: View {
+    let url: URL
+    let dismiss: () -> Void
+    
+    init(url: URL, dismiss: @escaping () -> Void) {
+        self.url = url
+        self.dismiss = dismiss
+    }
+    
+    var body: some View {
+        VStack {
+            Text(url.absoluteString)
+            Button("Close") { dismiss() }
+        }
+        .padding()
+    }
+}
 
-    @IBOutlet weak var imageView: UIImageView!
-
+final class ActionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Get the item[s] we're handling from the extension context.
         
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
-        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
-            for provider in item.attachments! {
-                if provider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    provider.loadItem(forTypeIdentifier: UTType.image.identifier) { (imageURL, error) in
-                        if let imageURL = imageURL as? URL {
-                            Task { @MainActor in
-                                if let strongImageView = weakImageView {
-                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-                                }
-                            }
-                        }
-                    }
-                    
-                    imageFound = true
-                    break
-                }
-            }
-            
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
+        guard let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
+              let itemProvider = extensionItem.attachments?.first else {
+            return close()
+        }
+        
+        itemProvider.loadItem(forTypeIdentifier: UTType.pdf.identifier) { (item, _) in
+            guard let url = item as? URL else { return self.close() }
+            self.performAction(on: url)
+        }
+
+        NotificationCenter.default.addObserver(
+            forName: NSNotification.Name("close"),
+            object: nil,
+            queue: nil
+        ) { _ in
+            DispatchQueue.main.async {
+                self.close()
             }
         }
     }
-
-    @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    
+    private func close() {
+        extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
 
+    static func postClose() {
+        NotificationCenter.default.post(name: NSNotification.Name("close"), object: nil)
+    }
+
+    private func performAction(on url: URL) {
+        DispatchQueue.main.async {
+            let ctrl = UIHostingController(rootView: ContentView(url: url, dismiss: Self.postClose))
+            self.addChild(ctrl)
+            self.view.addSubview(ctrl.view)
+
+            ctrl.view.translatesAutoresizingMaskIntoConstraints = false
+            ctrl.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+            ctrl.view.bottomAnchor.constraint (equalTo: self.view.bottomAnchor).isActive = true
+            ctrl.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+            ctrl.view.rightAnchor.constraint (equalTo: self.view.rightAnchor).isActive = true
+        }
+    }
 }
