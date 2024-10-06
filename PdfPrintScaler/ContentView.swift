b44cf44122e394ef67b9d5c2bcee2788cd61ca54
Stephen Bensley <stephen.e.bensley@gmail.com>
2024-10-06 13:13:23 -0700

    Add About page

--- PdfPrintScaler/ContentView.swift
+++ PdfPrintScaler/ContentView.swift
@@ -6,39 +6,61 @@
 //
 
 import SwiftUI
+import UtiliKit
+
+struct PdfPrintScalerInfo: AboutInfo {
+    let appStoreId: Int = 6575389687
+    let copyright: String = "© 2024 Stephen E. Bensley"
+    let description: String = "Scale & print PDF"
+    let gitHubAccount: String = "stephenbensley"
+    let gitHubRepo: String = "PdfPrintScaler"
+}
 
 struct ContentView: View {
     @State private var url: URL?
     @State private var pdf: ScalablePdf?
+    @State private var showAbout = false
     @State private var showError = false
     @State private var errorMessage = ""
-    
+
     var body: some View {
-        VStack {
-            Text("PDF Print Scaler")
-                .font(.title)
-            if let pdf = pdf {
-                ScalablePdfView(pdf: pdf, dismiss: clear)
-            } else {
-                PdfFilePicker(url: $url)
+        NavigationStack {
+            Group {
+                if let pdf = pdf {
+                    ScalablePdfView(pdf: pdf, dismiss: clear)
+                } else {
+                    PdfFilePicker(url: $url)
+                }
             }
-        }
-        .onChange(of: url) {
-            guard let url = url else { return }
-            do {
-                pdf = try ScalablePdf(url: url)
+            .navigationTitle("PDF Print Scaler")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button {
+                    showAbout = true
+                 } label: {
+                    Label("About", systemImage: "ellipsis.circle")
+                }
             }
-            catch {
-                errorMessage = error.localizedDescription
-                showError = true
+            .onChange(of: url) {
+                guard let url = url else { return }
+                do {
+                    pdf = try ScalablePdf(url: url)
+                }
+                catch {
+                    errorMessage = error.localizedDescription
+                    showError = true
+                }
             }
+            .sheet(isPresented: $showAbout) {
+                AboutView(info: PdfPrintScalerInfo())
+            }
+            .alert("Error Opening File", isPresented: $showError) {
+                Button("OK") { clear() }
+            } message: {
+                Text(errorMessage)
+            }
+            .padding()
         }
-        .alert("Error Opening File", isPresented: $showError) {
-            Button("OK") { clear() }
-        } message: {
-            Text(errorMessage)
-        }
-        .padding()
     }
     
     func clear() {
